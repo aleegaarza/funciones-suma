7238c54d33e16942b56669fec2240c107c27da7d
Alejandra Garza <aleegaarza@gmail.com>
2022-02-17 20:06:26 -0600

    fn suma added

---
+++ commits/script.js
@@ -0,0 +1,19 @@
+function obtenerMenor(x,y) {
+    return x > y ? x : y;
+
+}
+
+obtenerMenor(134,346);
+
+function suma(x,y) {
+    console.log(x+y); 
+}
+
+suma(38+23);
+
+
+function suma(x,y) {
+    console.log(x+y); 
+}
+
+suma(38+23);
